4461fca1520e7a60efb408b0d3b31aeeeb542e9e
BOADI DANSO <kwameadansonia123@gmail.com>
2024-03-31 19:59:25 +0000

    Added a new function called the odd_function. and called the functions

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # A program that print even numbers between one and 101.
-a = int(input("Enter the first number for the range."))
-b = int(input("Enter the last number for the range."))
+a = int(input("Enter the first number for the range. \n"))
+b = int(input("Enter the last number for the range. \n"))
 
 
 def even_numbers():
     for i in range(a, b):
         if i % 2 == 0:
             print(i)
+
+
+def odd_function():
+    for i in range(a, b):
+        if i % 2 != 0:
+            print(i)
+
+
+even_numbers()
+odd_function()
