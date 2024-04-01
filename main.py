212bc25720032b4da382128821b70edc25119b99
BOADI DANSO <kwameadansonia123@gmail.com>
2024-04-01 11:17:58 +0000

    Added a factorial function which will be modified to simplest form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # A program that print even numbers between one and 101.
+"""
 a = int(input("Enter the first number for the range. \n"))
 b = int(input("Enter the last number for the range. \n"))
 
@@ -12,8 +13,17 @@ def even_numbers():
 def odd_function():
     for i in range(a, b):
         if i % 2 != 0:
-            print(i)
+            print(i)"""
 
 
-even_numbers()
-odd_function()
+def factorial_function(n, a):
+    if n > 1:
+        while n > 1:
+            n -= 1
+            a *= n
+        print(a)
+    else:
+        print(1)
+
+
+factorial_function(5, 5)
